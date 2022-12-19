1ebb56477a4dbc96140e2d3f9798fb75f3a88fbf
gabrielefabro <fabro.1934872@studenti.uniroma1.it>
2022-12-19 11:44:53 +0100

    Commit

--- service/api/api.go
+++ service/api/api.go
@@ -38,10 +38,11 @@ package api
 
 import (
 	"errors"
-	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/database"
+	"net/http"
+
+	"git.gabrielefabro.it/service/database"
 	"github.com/julienschmidt/httprouter"
 	"github.com/sirupsen/logrus"
-	"net/http"
 )
 
 // Config is used to provide dependencies and configuration to the New function.
