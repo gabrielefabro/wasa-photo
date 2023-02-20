91ef0c58821b44ba51f22bccc954349d35d21a35
gabrielefabro <fabro.1934872@studenti.uniroma1.it>
2023-02-20 14:13:58 +0100

    fixed error on funciot GetUserProfile

--- service/database/Get-User-Profile.go
+++ service/database/Get-User-Profile.go
@@ -1,13 +1,19 @@
 package database
 
-func (db *appdbimpl) GetUserProfile (i id) (*Profile, error) {
+import (
+	"database/sql"
+	"errors"
+)
+
+func (db *appdbimpl) GetUserProfile(id uint64) (Profile, error) {
+	var ProfileNotFound = errors.New("Profile not found")
 	var profile Profile
 	query := "SELECT User, Bio, Posts, FollowingCount, FollowerCount FROM profile WHERE id = ?"
 	err := db.c.QueryRow(query, id).Scan(&profile.User, &profile.Bio, &profile.Posts, &profile.FollowingCount, &profile.FollowerCount)
 	if err == sql.ErrNoRows {
-		return nil, ProfileNotFound
+		return Profile{}, ProfileNotFound
 	} else if err != nil {
-		return nil, err
+		return Profile{}, err
 	}
-	return &profile, nil
+	return profile, nil
 }
