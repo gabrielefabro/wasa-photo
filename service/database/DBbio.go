32762c7dd4220e00390a6b2dc1dbe7abed8a40b3
gabrielefabro <fabro.1934872@studenti.uniroma1.it>
2023-04-12 15:32:46 +0200

    add function for the bio

---
+++ service/database/DBbio.go
@@ -0,0 +1,10 @@
+package database
+
+// Database function that add an bio to a profile that exists yet
+func (db *appdbimpl) SetBio(userID int, bio string) error {
+	_, err := db.c.Exec("UPDATE profiles SET bio=? WHERE user_id=?", bio, userID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
