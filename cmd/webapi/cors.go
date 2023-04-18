9172d2da595061e9846dfa8a54b25be978e41110
gabrielefabro <fabro.1934872@studenti.uniroma1.it>
2023-04-18 12:22:01 +0200

    fixed bug in ./cmd/webapi/cors.go

--- cmd/webapi/cors.go
+++ cmd/webapi/cors.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"github.com/gorilla/handlers"
 	"net/http"
+
+	"github.com/gorilla/handlers"
 )
 
 // applyCORSHandler applies a CORS policy to the router. CORS stands for Cross-Origin Resource Sharing: it's a security
@@ -13,7 +14,10 @@ func applyCORSHandler(h http.Handler) http.Handler {
 		handlers.AllowedHeaders([]string{
 			"x-example-header",
 		}),
+		handlers.AllowedHeaders([]string{"Content-Type", "Authorization", "X-Requested-With", "Accept", "Origin"}),
 		handlers.AllowedMethods([]string{"GET", "POST", "OPTIONS", "DELETE", "PUT"}),
 		handlers.AllowedOrigins([]string{"*"}),
+		handlers.ExposedHeaders([]string{"Autorization"}),
+		handlers.AllowCredentials(),
 	)(h)
 }
