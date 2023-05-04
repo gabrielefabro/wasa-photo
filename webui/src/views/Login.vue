1b69dee26ea1ad0429045acd471ee1b080e1d6ec
gabrielefabro <fabro.1934872@studenti.uniroma1.it>
2023-05-04 16:27:03 +0200

    loign.vue added

---
+++ webui/src/views/Login.vue
@@ -0,0 +1,53 @@
+<script>
+export default{
+	data() {
+		return {
+			errorMsg: "",
+			username: "",
+
+			testUsername: new RegExp('^[a-z0-9]*$')
+		}
+	},
+
+	methods: {
+		async doLogin() {
+			this.errorMsg = null;
+			try {
+				if(!this.testUsername.test(this.username)) throw "Invalid user, it should contain only lowcase letters or numbers"
+				if (this.username.length < 1 || this.username.length > 15) throw "Invalid username, it must contains mininum 1 characters and maximum 15 characters"
+				let response = await this.$axios.post('/session', {
+					username: this.username,
+				});
+				this.$router.replace("/home")
+				localStorage.userID = response.data.user.userID;
+                localStorage.username = response.data.user.username;
+			} catch (error) {
+				this.errorMsg = error.toString();
+				
+			}
+		},
+		mounted() {
+        localStorage.clear();
+    },
+	}
+	
+}
+
+</script>
+
+<template>
+	<ErrorMsg v-if="errorMsg" :msg="errorMsg" @close-error="errorMsg = ''"></ErrorMsg>
+	<div class="login-container">
+        <div class="title-login-container">
+            <span class="login-title"> Login </span>
+        </div>
+        <div class="box-container-login">
+            <span class="box-text-container-login">Username</span>
+            <input :on-submit="doLogin" type="text" name="username-form"  v-model="username" maxlength="15" placeholder="Your username" >
+        </div>
+        <div class="bottom-login-container">
+            <button @click="doLogin"> Login </button>
+        </div>
+
+    </div>
+</template>
