60243bc48938e10b040bbe3f5315fafa7c7ff8b2
gabrielefabro <fabro.1934872@studenti.uniroma1.it>
2023-05-12 18:45:34 +0200

    added components

--- webui/src/main.js
+++ webui/src/main.js
@@ -1,24 +1,32 @@
-import {createApp, reactive} from 'vue'
+import {
+    createApp,
+    reactive
+} from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from './services/axios.js';
-import LoadingSpinner from './components/LoadingSpinner.vue'
 import ErrorMsg from './components/ErrorMsg.vue'
+import LoadingSpinner from './components/LoadingSpinner.vue'
+import Navbar from './components/Navbar.vue'
 import Post from './components/Post.vue'
+import UserMiniCard from './components/UserMiniCard.vue'
+import LikeModal from './components/LikeModal.vue'
+import CommentModal from './components/CommentModal.vue'
+import PostComment from './components/PostComment.vue'
 
-import './assets/main.css'
-import './assets/login_style.css'
 import './assets/dashboard.css'
-
+import './assets/main.css'
 
 const app = createApp(App)
 app.config.globalProperties.$axios = axios;
 
-
 app.component("ErrorMsg", ErrorMsg);
-app.component("Post", Post);
 app.component("LoadingSpinner", LoadingSpinner);
-
-
+app.component("Navbar", Navbar);
+app.component("Post", Post);
+app.component("UserMiniCard", UserMiniCard);
+app.component("LikeModal", LikeModal);
+app.component("CommentModal", CommentModal);
+app.component("PostComment", PostComment);
 app.use(router)
 app.mount('#app')
