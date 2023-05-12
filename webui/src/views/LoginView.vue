dec286fff2043a3b51db1f623b330808fef1a70e
gabrielefabro <fabro.1934872@studenti.uniroma1.it>
2023-05-12 18:54:46 +0200

    new login

--- webui/src/views/LoginView.vue
+++ webui/src/views/LoginView.vue
@@ -1,38 +1,36 @@
 <script>
-export default{
-	data() {
+export default {
+	data: function() {
 		return {
-			errorMsg: "",
-			username: "",
-
-			testUsername: new RegExp('^[a-z0-9]*$')
+			errormsg: null,
+			identifier: "",
+			disabled: true,
 		}
 	},
-
 	methods: {
-		async doLogin() {
-			this.errorMsg = null;
+		async login() {
+			this.errormsg = null;
 			try {
-				if(!this.testUsername.test(this.username)) throw "Invalid user, it should contain only lowcase letters or numbers"
-				if (this.username.length < 1 || this.username.length > 15) throw "Invalid username, it must contains mininum 1 characters and maximum 15 characters"
-				let response = await this.$axios.post('/session', {
-					username: this.username,
+				let response = await this.$axios.post("/session",{
+					userId: this.identifier.trim()
 				});
+
+				localStorage.setItem('token',response.data.userId);
 				this.$router.replace("/home")
-				localStorage.userId = response.data.user.userId;
-                localStorage.username = response.data.user.username;
-			} catch (error) {
-				this.errorMsg = error.toString();
+				this.$emit('updatedLoggedChild',true)
 				
+			} catch (e) {
+				this.errormsg = e.toString();
 			}
 		},
-		mounted() {
-        localStorage.clear();
-    },
-	}
+	},
+	mounted(){
+		if (localStorage.getItem('token')){
+			this.$router.replace("/home")
+		}
+	},
 	
 }
-
 </script>
 
 <template>
@@ -40,13 +38,13 @@ export default{
 
 		<div class="row ">
 			<div class="col">
-				<ErrorMsg v-if="errorMsg" :msg="errorMsg"></ErrorMsg>
+				<ErrorMsg v-if="errormsg" :msg="errormsg"></ErrorMsg>
 			</div>
 		</div>
 
 		<div class="row h-100 w-100 m-0">
 			
-			<form @submit.prevent="doLogin" class="d-flex flex-column align-items-center justify-content-center p-0">
+			<form @submit.prevent="login" class="d-flex flex-column align-items-center justify-content-center p-0">
 
 				<div class="row mt-2 mb-3 border-bottom">
 					<div class="col">
@@ -60,15 +58,15 @@ export default{
 						type="text" 
 						class="form-control" 
 						v-model="identifier" 
-						maxlength="15"
+						maxlength="16"
 						minlength="1"
-						placeholder="Your username" />
+						placeholder="Your identifier" />
 					</div>
 				</div>
 
 				<div class="row mt-2 mb-5 ">
 					<div class="col ">
-						<button class="btn btn-dark" :disabled="identifier == null || identifier.length >16 || identifier.length <3 || identifier.trim().length<3"> 
+						<button class="btn btn-dark" :disabled="identifier == null || identifier.length >16 || identifier.length <1 || identifier.trim().length<1"> 
 						Register/Login 
 						</button>
 					</div>
@@ -79,6 +77,9 @@ export default{
 </template>
 
 <style>
+.login {
+    height: 100vh;
+}
 
 .login-title {
     color: black;
