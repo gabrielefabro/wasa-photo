bd7f01692425d7f189ab55abdd316e38c039d484
gabrielefabro <fabro.1934872@studenti.uniroma1.it>
2023-05-20 15:46:45 +0200

    login view upgrade

--- webui/src/views/LoginView.vue
+++ webui/src/views/LoginView.vue
@@ -35,18 +35,15 @@ export default {
 
 <template>
 	<div class="container-fluid h-100 m-0 p-0 login">
-
-		<div class="row ">
+		<div class="row">
 			<div class="col">
 				<ErrorMsg v-if="errorMsg" :msg="errorMsg" @close-error="errorMsg = ''"></ErrorMsg>
 			</div>
 		</div>
 
-		<div class="row h-100 w-100 m-0">
-			
-			<form @submit.prevent="login" class="d-flex flex-column align-items-center justify-content-center p-0">
-
-				<div class="row mt-2 mb-3 border-bottom">
+		<div class="row h-100 align-items-center justify-content-center">
+			<form @submit.prevent="login" class="d-flex flex-column align-items-center justify-content-center">
+				<div class="row mt-2 mb-3">
 					<div class="col">
 						<h2 class="login-title">WASAPhoto Login</h2>
 					</div>
@@ -55,19 +52,23 @@ export default {
 				<div class="row mt-2 mb-3">
 					<div class="col">
 						<input 
-						type="text" 
-						class="form-control" 
-						v-model="identifier" 
-						maxlength="16"
-						minlength="1"
-						placeholder="Your identifier" />
+							type="text" 
+							class="form-control" 
+							v-model="identifier" 
+							maxlength="16"
+							minlength="1"
+							placeholder="Your identifier" 
+						/>
 					</div>
 				</div>
 
-				<div class="row mt-2 mb-5 ">
-					<div class="col ">
-						<button class="btn btn-dark" :disabled="identifier == null || identifier.length >16 || identifier.length <1 || identifier.trim().length<1"> 
-						Register/Login 
+				<div class="row mt-2 mb-5">
+					<div class="col">
+						<button 
+							class="btn btn-primary" 
+							:disabled="identifier == null || identifier.length >16 || identifier.length <1 || identifier.trim().length<1"
+						> 
+							Register/Login 
 						</button>
 					</div>
 				</div>
@@ -78,10 +79,30 @@ export default {
 
 <style>
 .login {
-    height: 100vh;
+	height: 100vh;
+	background-image: url('../assets/photo/pixelfactory-instagram-cover.jpg');
+	background-size: cover;
+	background-position: center;
 }
 
 .login-title {
-    color: black;
+	color: #333;
+	font-size: 24px;
+}
+
+.form-control {
+	width: 300px;
+	border-color: #ccc;
+}
+
+.btn-primary {
+	background-color: #007bff;
+	border-color: #007bff;
+}
+
+.btn-primary:disabled {
+	background-color: #c3c3c3;
+	border-color: #c3c3c3;
+	cursor: not-allowed;
 }
 </style>
