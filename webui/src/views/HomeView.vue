f34c4f2d626aec5ea5f60598c6122fc1e97e9fb3
gabrielefabro <fabro.1934872@studenti.uniroma1.it>
2023-05-23 17:22:45 +0200

    frontend

--- webui/src/views/HomeView.vue
+++ webui/src/views/HomeView.vue
@@ -1,124 +1,77 @@
 <script>
 export default {
-  data: function () {
-    return {
-      errorMsg: "",
-      posts: [],
-    };
-  },
+	data: function () {
+		return {
+			errorMsg: "",
+			posts: [],
+		}
+	},
 
-  methods: {
-    async getMyStream() {
-      try {
-        this.errorMsg = null;
+	methods: {
+		async getMyStream() {
+			try {
+				this.errormsg = null;
 
-        let response = await this.$axios.get(
-          "/users/" + localStorage.getItem("token") + "/home"
-        );
+				let response = await this.$axios.get("/users/" + localStorage.getItem('token') + "/home");
 
-        if (response.data != null) {
-          this.posts = response.data;
-        }
-      } catch (error) {
-        this.errorMsg = this.$utils.errorToString(e);
-      }
-    },
+				if (response.data != null) {
+					this.posts = response.data;
+				}
+			} catch (error) {
+				this.errorMsg = this.$utils.errorToString(e);
+			}
+		},
 
-    async mounted() {
-      await this.getMyStream();
-    },
+		async mounted() {
+			await this.getMyStream();
+		},
 
-    goToSettingsPage() {
-      this.$router.push("/users/" + localStorage.getItem("token") + "/settings");
-    },
-
-    logout() {
-      // Implement your logout logic here
-    },
-  },
-};
+		goToSettingsPage() {
+			this.$router.push("/users/" + localStorage.getItem('token') + "/settings");
+		}
+	}
+}
 </script>
 
 <template>
-  <div class="container-fluid">
-    <ErrorMsg v-if="errorMsg" :msg="errorMsg" @close-error="errorMsg = ''"></ErrorMsg>
-    <div class="row">
-      <post
-        v-for="(post, index) in posts"
-        :key="index"
-        :username="post.username"
-        :postId="post.postId"
-        :comments="post.comments != null ? post.comments : []"
-        :likes="post.likes != null ? post.likes : []"
-        :publication_time="post.publication_time"
-      />
-    </div>
-    <span v-if="posts.length === 0" class="no-posts-text">There are no posts yet,</span>
-    <span v-if="posts.length === 0" class="no-posts-text fw-500 fs-6">start to follow someone!</span>
+	<div class="container-fluid">
+		<ErrorMsg v-if="errorMsg" :msg="errorMsg" @close-error="errorMsg = ''"></ErrorMsg>
+		<div class="row">
+			<post
+				v-for="(post,index) in posts"
+				:key="index"
+				:username="post.username"
+				:postId="post.postId"
+				:comments="post.comments != nil ? post.comments : []"
+				:likes="post.likes != nil ? post.likes : []"
+				:pubblication_time="post.pubblication_time"
+			/>
+		</div>
+		<span v-if="posts.length == 0" class="no-posts-text"> There are no posts yet, </span>
+		<span v-if="posts.length == 0" class="no-posts-text fw-500 fs-6"> start to follow someone!</span>
 
-    <!-- Pulsante per andare al profilo -->
-    <button class="profile-button" @click="goToSettingsPage">
-      <i class="fa fa-user"></i>
-    </button>
-
-    <!-- Pulsante di logout -->
-    <button class="logout-button" @click="logout">
-      <i class="fa fa-sign-out"></i>
-    </button>
-  </div>
+		<!-- Aggiunta del pulsante delle impostazioni -->
+		<button class="settings-button" @click="goToSettingsPage">
+			<i class="fa fa-cog"></i>
+		</button>
+	</div>
 </template>
 
 <style>
 .settings-button {
-  position: fixed;
-  bottom: 20px;
-  right: 20px;
-  background-color: #fff;
-  border: none;
-  border-radius: 50%;
-  width: 40px;
-  height: 40px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  cursor: pointer;
+	position: fixed;
+	bottom: 20px;
+	right: 20px;
+	background-color: #fff;
+	border: none;
+	border-radius: 50%;
+	width: 40px;
+	height: 40px;
+	box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+	cursor: pointer;
 }
 
 .settings-button i {
-  font-size: 20px;
-}
-
-.profile-button,
-.logout-button {
-  position: fixed;
-  bottom: 20px;
-  right: 80px;
-  background-color: #fff;
-  border: none;
-  border-radius: 50%;
-  width: 40px;
-  height: 40px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  cursor: pointer;
-}
-
-.profile-button i,
-.logout-button i {
-  font-size: 20px;
-}
-
-.profile-button {
-  background-color: #3498db;
-  color: #fff;
-}
-
-.logout-button {
-  background-color: #e74c3c;
-  color: #fff;
-}
-
-.no-posts-text {
-  color: #555;
-  display: block;
-  text-align: center;
-  margin-top: 20px;
+	font-size: 20px;
 }
 </style>
