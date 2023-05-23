e64d980185f06aab944d0beeaa84edb8f3146ca9
gabrielefabro <fabro.1934872@studenti.uniroma1.it>
2023-05-23 17:46:40 +0200

    frontend

--- webui/src/views/ProfileView.vue
+++ webui/src/views/ProfileView.vue
@@ -174,52 +174,57 @@ export default {
   },
 };
 </script>
-
 <template>
-  <div class="container-fluid" v-if="!currentIsBanned && userExists">
+  <div class="container-fluid profile-page" v-if="!currentIsBanned && userExists">
     <div class="row">
       <div class="col-12 d-flex justify-content-center">
-        <div class="card w-50 container-fluid">
+        <div class="card profile-card">
           <div class="row">
             <div class="col">
               <div class="card-body d-flex justify-content-between align-items-center">
-                <h5 class="card-title p-0 me-auto mt-auto">
-                  {{ userName }} @{{ this.$route.params.user_id }}
-                </h5>
-                <button
-                  v-if="!sameUser && !banStatus"
-                  @click="followClick"
-                  class="btn btn-success ms-2"
-                >
-                  {{ followStatus ? "Unfollow" : "Follow" }}
-                </button>
-                <button
-                  v-if="!sameUser"
-                  @click="banClick"
-                  class="btn btn-danger ms-2"
-                >
-                  {{ banStatus ? "Unban" : "Ban" }}
-                </button>
-                <button
-                  v-else
-                  class="my-trnsp-btn ms-2"
-                  @click="goToSettings"
-                >
-                  <i class="my-nav-icon-gear fa-solid fa-gear"></i>
-                </button>
+                <div class="profile-info">
+                  <h5 class="profile-username">{{ userName }}</h5>
+                  <h6 class="profile-userid">@{{ this.$route.params.user_id }}</h6>
+                </div>
+                <div>
+                  <button
+                    v-if="!sameUser && !banStatus"
+                    @click="followClick"
+                    class="btn btn-follow"
+                  >
+                    {{ followStatus ? "Following" : "Follow" }}
+                  </button>
+                  <button
+                    v-if="!sameUser"
+                    @click="banClick"
+                    class="btn btn-ban"
+                  >
+                    {{ banStatus ? "Unban" : "Ban" }}
+                  </button>
+                  <button
+                    v-else
+                    class="btn btn-settings"
+                    @click="goToSettings"
+                  >
+                    <i class="fa-solid fa-gear"></i>
+                  </button>
+                </div>
               </div>
             </div>
           </div>
 
-          <div v-if="!banStatus" class="row mt-1 mb-1">
-            <div class="col-4 d-flex justify-content-start">
-              <h6 class="ms-3 p-0 ">Posts: {{ postCnt }}</h6>
+          <div v-if="!banStatus" class="row profile-stats">
+            <div class="col-4">
+              <h6 class="stat-label">Posts</h6>
+              <h6 class="stat-count">{{ postCnt }}</h6>
             </div>
-            <div class="col-4 d-flex justify-content-center">
-              <h6 class=" p-0 ">Followers: {{ followerCnt }}</h6>
+            <div class="col-4">
+              <h6 class="stat-label">Followers</h6>
+              <h6 class="stat-count">{{ followerCnt }}</h6>
             </div>
-            <div class="col-4 d-flex justify-content-end">
-              <h6 class=" p-0 me-3">Following: {{ followingCnt }}</h6>
+            <div class="col-4">
+              <h6 class="stat-label">Following</h6>
+              <h6 class="stat-count">{{ followingCnt }}</h6>
             </div>
           </div>
         </div>
@@ -228,7 +233,7 @@ export default {
 
     <div class="row">
       <div class="container-fluid mt-3">
-        <div class="row ">
+        <div class="row">
           <div class="col-12 d-flex justify-content-center">
             <h2>Posts</h2>
             <input
@@ -240,15 +245,15 @@ export default {
             />
             <label
               v-if="sameUser"
-              class="btn my-btn-add-photo ms-2 d-flex align-items-center"
+              class="btn btn-add-photo"
               for="fileUploader"
             >
-              Add
+              Add Photo
             </label>
           </div>
         </div>
 
-        <div class="row ">
+        <div class="row">
           <div class="col-3"></div>
           <div class="col-6">
             <hr class="border border-dark" />
@@ -273,10 +278,8 @@ export default {
           />
         </div>
 
-        <div v-else class="mt-5 ">
-          <h2 class="d-flex justify-content-center" style="color: white;">
-            No posts yet
-          </h2>
+        <div v-else class="mt-5 no-posts-message">
+          <h2>No posts yet</h2>
         </div>
       </div>
     </div>
@@ -286,26 +289,98 @@ export default {
 </template>
 
 <style>
-.profile-file-upload {
-  display: none;
+.profile-page {
+  background-color: #fafafa;
+  padding-top: 20px;
 }
 
-.my-nav-icon-gear {
-  color: grey;
+.profile-card {
+  width: 50%;
 }
 
-.my-nav-icon-gear:hover {
+.profile-username {
+  font-size: 24px;
+  font-weight: bold;
+}
+
+.profile-userid {
+  font-size: 16px;
+  color: gray;
+  margin-top: 5px;
+}
+
+.btn-follow {
+  background-color: #3897f0;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 14px;
+  font-weight: bold;
+}
+
+.btn-ban {
+  background-color: #ed4956;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 14px;
+  font-weight: bold;
+}
+
+.btn-settings {
+  background-color: transparent;
+  border: none;
+  padding: 8px;
+  cursor: pointer;
+}
+
+.profile-stats {
+  margin-top: 10px;
+  margin-bottom: 20px;
+}
+
+.stat-label {
+  font-size: 14px;
+  font-weight: bold;
+  color: gray;
+}
+
+.stat-count {
+  font-size: 18px;
+  font-weight: bold;
+}
+
+.btn-add-photo {
+  background-color: #3897f0;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 14px;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+.no-posts-message {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 200px;
+  background-color: white;
+  border-radius: 4px;
+  color: gray;
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.fa-gear {
+  color: gray;
+}
+
+.fa-gear:hover {
   transform: scale(1.3);
 }
-
-.my-btn-add-photo {
-  background-color: green;
-  border-color: grey;
-}
-
-.my-btn-add-photo:hover {
-  color: white;
-  background-color: green;
-  border-color: grey;
-}
 </style>
+
