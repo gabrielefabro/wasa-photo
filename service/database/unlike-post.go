aac4c221da6ecf6d6122c23db5ba8b15312dbfca
gabrielefabro <fabro.1934872@studenti.uniroma1.it>
2023-05-27 14:20:43 +0200

    BACKEND

--- service/database/unlike-post.go
+++ service/database/unlike-post.go
@@ -5,7 +5,7 @@ func (db *appdbimpl) UnlikePost(postId PostId, user User) error {
 
 	var query = "DELETE FROM likes WHERE(post_id = ? AND user_id = ?)"
 
-	_, err := db.c.Exec(query, postId, user.User_id)
+	_, err := db.c.Exec(query, postId.Post_id, user.User_id)
 	if err != nil {
 		return err
 	}
