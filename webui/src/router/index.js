3f3c492e3bb5f7b3213cd86e7002bda6fa94ad2d
gabrielefabro <fabro.1934872@studenti.uniroma1.it>
2023-05-27 17:34:36 +0200

    FRONTEND

--- webui/src/router/index.js
+++ webui/src/router/index.js
@@ -2,6 +2,8 @@ import {createRouter, createWebHashHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import ProfileView from '../views/ProfileView.vue'
+import SettingsView from '../views/SettingsView.vue'
+import SearchView from '../views/SearchView.vue'
 
 const router = createRouter({
 	history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -10,6 +12,8 @@ const router = createRouter({
 		{path: '/login', component: LoginView},
 		{path: '/', redirect: '/login'},
 		{path: '/users/:user_id', component: ProfileView},
+		{path: '/search', component: SearchView},
+		{path: '/users/user_id/settings', component: SettingsView}
 	]
 })
 
