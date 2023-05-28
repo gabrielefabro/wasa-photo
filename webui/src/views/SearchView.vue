76a74aa190538a325d5571c6528633d359f9f465
gabrielefabro <fabro.1934872@studenti.uniroma1.it>
2023-05-28 14:20:31 +0200

    FRONTEND

--- webui/src/views/SearchView.vue
+++ webui/src/views/SearchView.vue
@@ -30,6 +30,7 @@ export default {
 				let response = await this.$axios.get("/users",{
 						params: {
 						user_id: this.searchValue,
+						username: this.username
 					},
 				});
 				this.users = response.data
@@ -58,8 +59,8 @@ export default {
 	<div class="container-fluid h-100 ">
 		<UserMiniCard v-for="(user,index) in users" 
 		:key="index"
-		:identifier="user.user_id" 
-		:nickname="user.nickname" 
+		:user_id="user.user_id" 
+		:username="user.nickname" 
 		@clickedUser="goToProfile"/>
 
 		<p v-if="users.length == 0" class="no-result-text d-flex justify-content-center"> No users found.</p>
