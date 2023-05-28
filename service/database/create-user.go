8e6558dad75b205f2fb44c49ba07294f4b8fa6a6
gabrielefabro <fabro.1934872@studenti.uniroma1.it>
2023-05-28 11:23:13 +0200

    BACKEND

--- service/database/create-user.go
+++ service/database/create-user.go
@@ -1,11 +1,11 @@
 package database
 
 // Function that adds a new user in the database
-func (db *appdbimpl) CreateUser(userId userId) error {
+func (db *appdbimpl) CreateUser(userid userId) error {
 
 	var query = "INSERT INTO users (user_id,username) VALUES (?, ?)"
 
-	_, err := db.c.Exec(query, userId.User_id, userId.User_id)
+	_, err := db.c.Exec(query, userid.User_id, userid.User_id)
 
 	if err != nil {
 		return err
