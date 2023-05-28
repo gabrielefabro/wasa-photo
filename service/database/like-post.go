3d67462c183b5d10b88cf24694d5ddfc5a46bfe5
gabrielefabro <fabro.1934872@studenti.uniroma1.it>
2023-05-28 13:25:31 +0200

    BACKEND

--- service/database/like-post.go
+++ service/database/like-post.go
@@ -5,7 +5,7 @@ func (db *appdbimpl) LikePost(postId PostId, user User) error {
 
 	var query = "INSERT INTO likes (post_id,user_id,username) VALUES (?, ?, ?)"
 
-	_, err := db.c.Exec(query, postId, user.User_id, user.Username)
+	_, err := db.c.Exec(query, postId.Post_id, user.User_id, user.Username)
 	if err != nil {
 		return err
 	}
