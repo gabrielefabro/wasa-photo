95d614b7b70af00aee6b1e1596f907d5184381ec
gabrielefabro <fabro.1934872@studenti.uniroma1.it>
2023-05-28 13:48:40 +0200

    FRONTEND

--- webui/src/components/UserMiniCard.vue
+++ webui/src/components/UserMiniCard.vue
@@ -10,35 +10,44 @@ export default {
     },
 }
 </script>
-
 <template>
-    
     <div class="row mb-2 mt-2">
-        <div class="col d-flex justify-content-center">
-            <div class="user-mini-card card bg-transparent border-start">
-                <div class="card-body">
-                    <h5 @click="clickedUser" class="user-mini-card-title d-flex justify-content-center ">{{username }} @{{user_id }}</h5>
-                </div>
-            </div>
+      <div class="col d-flex justify-content-center">
+        <div class="user-mini-card card bg-light border-start">
+          <div class="card-body">
+            <h5 @click="clickedUser" class="user-mini-card-title d-flex justify-content-center">{{ username }} @{{ user_id }}</h5>
+          </div>
         </div>
+      </div>
     </div>
-    
-</template>
-
-<style>
-.user{
-    color: var(--color-text);
-}
-
-.user-mini-card{
-    color: black;
+  </template>
+  
+  <style>
+  .user {
+    color: #333;
+  }
+  
+  .user-mini-card {
+    color: #333;
     width: 25%;
     border: none;
-    border-color: black;
-}
-
-.user-mini-card-title:hover{
+    border-color: #333;
+    transition: all 0.3s;
+  }
+  
+  .user-mini-card:hover {
+    transform: translateY(-3px);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  }
+  
+  .user-mini-card-title {
     cursor: pointer;
     font-weight: bold;
-}
-</style>
+    transition: all 0.3s;
+  }
+  
+  .user-mini-card-title:hover {
+    color: #007bff;
+  }
+  </style>
+  
