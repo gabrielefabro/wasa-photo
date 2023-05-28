fd271d3dbf71b70ee680dfedb4366914af29f809
gabrielefabro <fabro.1934872@studenti.uniroma1.it>
2023-05-28 11:22:41 +0200

    BACKEND

--- service/database/create-user.go
+++ service/database/create-user.go
@@ -5,7 +5,7 @@ func (db *appdbimpl) CreateUser(userId userId) error {
 
 	var query = "INSERT INTO users (user_id,username) VALUES (?, ?)"
 
-	_, err := db.c.Exec(query, userId, userId)
+	_, err := db.c.Exec(query, userId.User_id, userId.User_id)
 
 	if err != nil {
 		return err
