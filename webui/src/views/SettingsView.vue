5bde65df0ecffa71a08956dd48684628aaf358d2
gabrielefabro <fabro.1934872@studenti.uniroma1.it>
2023-05-28 13:38:06 +0200

    FRONTEND

--- webui/src/views/SettingsView.vue
+++ webui/src/views/SettingsView.vue
@@ -25,58 +25,66 @@ export default {
 </script>
 
 <template>
-	<div class="container-fluid">
-		<div class="row">
-			<div class="col d-flex justify-content-center mb-2">
-				<h1>{{ this.$route.params.user_id }}'s Settings</h1>
-			</div>
+	<div class="container">
+	  <div class="row">
+		<div class="col d-flex justify-content-center mb-4">
+		  <h1>{{ this.$route.params.user_id }}'s Settings</h1>
 		</div>
-
-		<div class="row ">
-			<div class="col-12 d-flex justify-content-center">
-				<p class="me-1" style="color: var(--color-red-danger);">[Disclaimer] </p> <p> A user has this structure: </p> <p class="ms-1 me-1" style="color: green;">username</p> <p> @identifier. </p>
-			</div>
-			<div class="col-12 d-flex justify-content-center">
-				<p>It's only possible to modify the part before the @</p> (the <p class="ms-1 me-1" style="color: green;">Username</p>) <p>and not the one after (the identifier of the user) </p>
-			</div>
-			<div class="col-12 d-flex justify-content-center">
-				<p>Username has been intepreted as a nickname (they're the same thing).  </p>
-			</div>
+	  </div>
+  
+	  <div class="row">
+		<div class="col-12 d-flex justify-content-center">
+		  <p class="text-danger me-1">[Disclaimer]</p>
+		  <p>A user has the following structure:</p>
+		  <p class="text-success ms-1 me-1">username</p>
+		  <p>@identifier.</p>
 		</div>
-
-		<div class="row mt-2">
-			<div class="col d-flex justify-content-center">
-				<div class="input-group mb-3 w-25">
-					<input
-						type="text"
-						class="form-control w-25"
-						placeholder="Your new username..."
-						maxlength="15"
-						minlength="1"
-						v-model="username"
-					/>
-					<div class="input-group-append">
-						<button class="btn btn-outline-secondary" 
-						@click="modifyNickname"
-						:disabled="username === null || username.length >15 || username.length <1 || username.trim().length===0">
-						Modify</button>
-					</div>
-				</div>
+		<div class="col-12 d-flex justify-content-center">
+		  <p>You can only modify the part before the @ symbol (the <span class="text-success">Username</span>) and not the identifier of the user.</p>
+		</div>
+		<div class="col-12 d-flex justify-content-center">
+		  <p>The term "Username" refers to a nickname.</p>
+		</div>
+	  </div>
+  
+	  <div class="row mt-3">
+		<div class="col d-flex justify-content-center">
+		  <div class="input-group mb-3 w-50">
+			<input
+			  type="text"
+			  class="form-control"
+			  placeholder="Enter your new username..."
+			  maxlength="15"
+			  minlength="1"
+			  v-model="username"
+			/>
+			<div class="input-group-append">
+			  <button
+				class="btn btn-primary"
+				@click="modifyNickname"
+				:disabled="!username || username.length > 15 || username.length < 1 || username.trim().length === 0"
+			  >
+				Modify
+			  </button>
 			</div>
+		  </div>
 		</div>
-
-		<div class="row" >
-			<div v-if="username.trim().length>0" class="col d-flex justify-content-center">
-				Preview: {{username}} @{{ this.$route.params.user_id }}
-			</div>
+	  </div>
+  
+	  <div class="row">
+		<div v-if="username.trim().length > 0" class="col d-flex justify-content-center mt-2">
+		  Preview: {{ username }}@{{ this.$route.params.user_id }}
 		</div>
-
-		<div class="row">
-			<ErrorMsg v-if="errormsg" :msg="errormsg"></ErrorMsg>
+	  </div>
+  
+	  <div class="row mt-4">
+		<div class="col d-flex justify-content-center">
+		  <ErrorMsg v-if="errormsg" :msg="errormsg"></ErrorMsg>
 		</div>
+	  </div>
 	</div>
-	
-</template>
+  </template>
+  
 
 <style>
 </style>
