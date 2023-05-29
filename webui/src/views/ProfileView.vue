3e3f8788e250ac054b6bc5f98f13df4cfdf8503b
gabrielefabro <fabro.1934872@studenti.uniroma1.it>
2023-05-29 12:21:04 +0200

    FRONTEND

--- webui/src/views/ProfileView.vue
+++ webui/src/views/ProfileView.vue
@@ -39,27 +39,47 @@ export default {
   methods: {
     
     async uploadFile() {
-      let fileInput = document.getElementById("fileUploader");
+      const file = this.$refs.file.files[0];
 
-      const file = fileInput.files[0];
-      const reader = new FileReader();
+      // Check file type, only jpg and jpeg are allowed
+      const fileType = file.type;
+      if (fileType !== "image/jpeg") {
+        this.errorMsg = "File type not supported, only jpg and jpeg are allowed";
+        document.querySelector('.drag-drop-area').style.backgroundColor = "#FF8989";
+       return
+      }
 
-      reader.readAsArrayBuffer(file);
+      // Check file size, max 5MB
+      const fileSize = file.size;
+      if (fileSize > 5242880) {
+        this.errorMsg = "File size is too big. Max size is 5MB";
+        document.querySelector('.drag-drop-area').style.backgroundColor = "#FF8989";
+        return
+      }
 
-      reader.onload = async () => {
-        let response = await this.$axios.post(
-          "/users/" + this.$route.params.user_id + "/posts",
-          reader.result,
-          {
-            headers: {
-              "Content-Type": 'multipart/form-data',
-            },
-          }
-        );
-        this.posts.unshift(response.data);
-        this.postCnt += 1;
-      };
+      // Convert file to base64
+      this.file64 = URL.createObjectURL(file);
     },
+
+    async createPost(postData) {
+      const formData = new FormData();
+      formData.append("image", postData['imageFile']);
+      formData.append("caption", postData['caption']);
+
+      try {
+        let _ = await this.$axios.post("/users/" +
+              this.$route.params.user_id + "/posts/", formData, {
+        headers: {
+          'content-type': 'multipart/form-data'
+        }
+        });
+                this.$emit('refresh-data')
+                setTimeout(this.$emit('exit-upload-form'), 1000);
+            } catch (e) {
+                this.errorMsg = this.$utils.errorToString(e);;
+                this.$emit('error-occurred', this.errorMsg);
+            }
+        },
 
     async followClick() {
       try {
