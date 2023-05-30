ae6d759449aaedbbde4a90399266821f74d3ccbb
gabrielefabro <fabro.1934872@studenti.uniroma1.it>
2023-05-30 18:52:38 +0200

    BACKEND

--- service/database/get-username.go
+++ service/database/get-username.go
@@ -6,7 +6,7 @@ func (db *appdbimpl) GetUserName(userId UserId) (string, error) {
 	var username string
 	var query = "SELECT username FROM users WHERE user_id = ?"
 
-	err := db.c.QueryRow(query, userId).Scan(&username)
+	err := db.c.QueryRow(query, userId.User_id).Scan(&username)
 	if err != nil {
 		return "", err
 	}
